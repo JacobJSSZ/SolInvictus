0dbe411732c6d4cb47deac97ffa13eb9617280b0
Jakub <imraszun@gmail.com>
2023-10-06 20:26:13 +0200

    Dodaj pliki projektów.

---
+++ SolInvictus/Program.cs
@@ -0,0 +1,17 @@
+﻿var liczba = 4634655888999;
+int abundance;
+var liczbaAsString = liczba.ToString();
+char[] warred = liczbaAsString.ToArray();
+
+for (char a = '0'; a <= '9'; a++)
+{
+    abundance = 0;
+    for (int g = 0; g < warred.Length; g += 1)
+    {
+        if (warred[g] == a)
+        {
+            abundance++;
+        }
+    }
+    Console.WriteLine(a.ToString() + "=>" + abundance.ToString());
+}
